b8fd1ec9a5f08c3d0e667079f12288ec5e8869eb
sedat <sdinc00@gmail.com>
2022-12-12 16:29:33 +0200

    day10 oop 1- tva

--- src/day10oop/main.java
+++ src/day10oop/main.java
@@ -4,6 +4,11 @@ public class main {
     public static void main(String[] args) {
         pen p1= new pen();
         salary s1= new salary();
+        tva tva1 =new tva();
+
+        System.out.println("motorina 100lei cu tva="+tva1.motorina(100));
+        System.out.println("motorina 20lei cu tva="+tva1.book(20));
+
 
         System.out.println(s1.yearMoreThen10(1200));
         System.out.println(s1.yearLessThen10(1200));
---
+++ src/day10oop/tva.java
@@ -0,0 +1,12 @@
+package day10oop;
+
+public class tva {
+    static public double motorina(int a){
+        return a+a*0.19;
+
+    }
+    static public double book(int b){
+        return b+b*0.10;
+
+    }
+}
